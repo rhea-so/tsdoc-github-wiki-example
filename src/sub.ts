90072b9902e2639fbb9d8977a490acbbe184b90c
JeongHyeon Kim <jeonghyeon.rhea@gmail.com>
2024-01-24 20:29:23 +0900

    Add sum, sub

---
+++ src/sub.ts
@@ -0,0 +1,24 @@
+/**
+ * Subtracts two numbers.
+ * @param x The first number.
+ * @param y The second number.
+ * @returns The difference of the two numbers.
+ * @example
+ * ```typescript
+ * // Return 0:
+ * sub(1, 1);
+ * ```
+ * @example
+ * ```typescript
+ * // Return 2:
+ * sub(3, 1);
+ * ```
+ * @example
+ * ```typescript
+ * // Return -2:
+ * sub(1, 3);
+ * ```
+ */
+export function sub(x: number, y: number): number {
+  return x - y;
+}
---
+++ src/sum.ts
@@ -0,0 +1,19 @@
+/**
+ * Adds two numbers.
+ * @param x The first number.
+ * @param y The second number.
+ * @returns The sum of the two numbers.
+ * @example
+ * ```typescript
+ * // Return 2:
+ * add(1, 1);
+ * ```
+ * @example
+ * ```typescript
+ * // Return 4:
+ * add(3, 1);
+ * ```
+ */
+export function add(x: number, y: number): number {
+  return x + y;
+}
